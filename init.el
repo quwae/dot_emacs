7b62b6305a3017ee48fabb975cb9831975dfa431
Tohru Quwae <homer.quwae+github@gmail.com>
2022-11-17 16:15:02 +0900

    install company

---
+++ init.el
@@ -0,0 +1,19 @@
+;; package
+(require 'package)
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+	("org"   . "https://orgmode.org/elpa/")
+	("gnu"   . "https://elpa.gnu.org/packages")))
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
