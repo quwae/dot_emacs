89588b66a442810b1b54d63f7ea8567ab0d044f0
Tohru Quwae <homer.quwae+github@gmail.com>
2022-12-05 19:46:35 +0900

    add 'melpa-stable' package archive

--- init.el
+++ init.el
@@ -1,9 +1,13 @@
 ;; package
 (require 'package)
-(setq package-archives
-      '(("melpa" . "https://melpa.org/packages/")
-	("org"   . "https://orgmode.org/elpa/")
-	("gnu"   . "https://elpa.gnu.org/packages")))
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("org"   . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives
+	     '("gnu"   . "https://elpa.gnu.org/packages") t)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
