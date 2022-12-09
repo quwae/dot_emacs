4804bb58aa6a40a4e019040c7f5d8d3de74a5acd
Tohru Quwae <homer.quwae+github@gmail.com>
2022-12-09 17:48:04 +0900

    install lsp-mode

--- init.el
+++ init.el
@@ -15,7 +15,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(misterioso))
- '(package-selected-packages '(magit company)))
+ '(package-selected-packages '(lsp-mode magit company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
